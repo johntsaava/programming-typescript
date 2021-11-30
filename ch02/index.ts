6a81f277df5cb37340a1d76c6505e3a0e692e6e8
John Tsaava <johntsaava@gmail.com>
2021-11-30 20:25:37 +0400

    ch08 p8

---
+++ ch02/index.ts
@@ -0,0 +1,11 @@
+// Compiling and running TypeScript
+
+// ts
+// 1. ts source -> ts AST
+// 2. AST is checked by typechecker
+// 3. ts AST -> js source
+
+// js
+// 4. js source -> js AST
+// 5. AST -> bytecode
+// 6. bytecode is evaluated by runtime
