f49752ee5fa784d95d52c28d3e4544814d5b1c30
John Tsaava <johntsaava@gmail.com>
2021-12-02 08:52:21 +0400

    ch03 p17

--- ch02/src/index.ts
+++ ch02/src/index.ts
@@ -9,3 +9,11 @@
 // 4. js source -> js AST
 // 5. AST -> bytecode
 // 6. bytecode is evaluated by runtime
+
+let a = 1 + 2;
+let b = a + 3;
+let c = {
+  apple: a,
+  banana: b,
+};
+let d = c.apple * 4;
